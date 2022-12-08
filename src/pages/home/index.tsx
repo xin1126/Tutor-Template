42dce0762d9d5beb08e7cd282aea9ae38c7177b2
xin1126 <z26319491z@gmail.com>
2022-12-08 11:09:11 +0800

    feat: images

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,7 @@
 import logo from '@/assets/logo.svg'
 
 const Home: React.FC = () => {
-  return (
-    <div className="flex flex-col items-center text-5xl">
-      <p>首頁</p>
-      <img className="w-2/4" src={logo} alt="logo" />
-    </div>
-  )
+  return <div className="flex flex-col items-center text-5xl"></div>
 }
 
 export default Home
