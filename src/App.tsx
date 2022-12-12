df9d2051f40b089b9306c3afa4a8d6bec12ea507
xin1126 <z26319491z@gmail.com>
2022-12-12 17:24:49 +0800

    feat: Aos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { HashRouter, useRoutes } from 'react-router-dom'
 import routes from './routes'
 // import Footer from '@/components/Footer'
 import Header from '@/components/Header'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const Element: React.FC = () => {
   const element = useRoutes(routes)
@@ -10,6 +12,13 @@ const Element: React.FC = () => {
 }
 
 const App: React.FC = () => {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      duration: 1500,
+    })
+    AOS.refresh()
+  }, [])
   return (
     <HashRouter>
       <Header />
