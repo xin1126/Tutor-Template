1119ceb391cbfb495d149d37c23b393f415bacfa
xin1126 <z26319491z@gmail.com>
2022-12-13 19:08:00 +0800

    fix: Title components

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,12 +1,15 @@
 interface Props {
   title: string
+  subtitle?: string
 }
 
-const Title: React.FC<Props> = ({ title }) => {
+const Title: React.FC<Props> = ({ title, subtitle }) => {
   return (
     <>
       <h2 className="mb-4 pt-12 text-center text-2xl text-white md:text-3xl lg:pt-20">
         {title}
+        <br />
+        {subtitle}
       </h2>
       <div className="mx-auto mb-6 w-[60px] border-b-4 border-secondary sm:mb-12"></div>
     </>
