8cc21a34ecefc8f6ddce56a339c90f37262f0dd6
xin1126 <z26319491z@gmail.com>
2022-12-13 10:04:04 +0800

    feat: lib images

---
+++ src/lib/images.tsx
@@ -0,0 +1,45 @@
+import teacher01 from '@/assets/images/teacher01.jpg'
+import teacher02 from '@/assets/images/teacher02.jpg'
+import teacher03 from '@/assets/images/teacher03.jpg'
+import teacher04 from '@/assets/images/teacher04.jpg'
+import teacher05 from '@/assets/images/teacher05.jpg'
+import teacher06 from '@/assets/images/teacher06.jpg'
+
+import student01 from '@/assets/images/student01.jpg'
+import student02 from '@/assets/images/student02.jpg'
+import student03 from '@/assets/images/student03.jpg'
+import student04 from '@/assets/images/student04.jpg'
+
+import article01 from '@/assets/images/article01.jpg'
+import article02 from '@/assets/images/article02.jpg'
+import article03 from '@/assets/images/article03.jpg'
+
+import info01 from '@/assets/images/info01.jpg'
+import info02 from '@/assets/images/info02.jpg'
+import info03 from '@/assets/images/info03.jpg'
+import info04 from '@/assets/images/info04.jpg'
+
+const images = {
+  teacher01,
+  teacher02,
+  teacher03,
+  teacher04,
+  teacher05,
+  teacher06,
+
+  student01,
+  student02,
+  student03,
+  student04,
+
+  article01,
+  article02,
+  article03,
+
+  info01,
+  info02,
+  info03,
+  info04,
+}
+
+export default images
