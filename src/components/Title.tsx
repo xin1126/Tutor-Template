3916957b21da906214741df733bb115db06e35b7
xin1126 <z26319491z@gmail.com>
2022-12-13 11:49:11 +0800

    fix: Title Components Responsive

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -8,7 +8,7 @@ const Title: React.FC<Props> = ({ title }) => {
       <h2 className="mb-4 pt-12 text-center text-2xl text-white md:text-3xl lg:pt-20">
         {title}
       </h2>
-      <div className="mx-auto mb-12 w-[60px] border-b-4 border-secondary"></div>
+      <div className="mx-auto mb-6 w-[60px] border-b-4 border-secondary sm:mb-12"></div>
     </>
   )
 }
