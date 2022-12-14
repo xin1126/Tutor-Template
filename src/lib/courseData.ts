098e5ea3f4dc5cc71e1bd30686935518f9ce07ce
xin1126 <z26319491z@gmail.com>
2022-12-14 17:06:10 +0800

    fix: PopularCourses 資料抽離至 global，轉導課程介紹

---
+++ src/lib/courseData.ts
@@ -0,0 +1,46 @@
+import images from '@/lib/images'
+
+const data = [
+  {
+    avatar: images.teacher01,
+    name: 'Belinda',
+    title: '基礎前端 - HTML',
+    type: '前端語言',
+    content:
+      '帶領無基礎的平民老百姓，從最基礎的 HTML 語法出發，使用最淺顯易懂的語言與生活隨處可見的例子，輕鬆進入網頁架設的世界，完成一個基礎的網頁架設。',
+    time: '50分鐘',
+    price: 'NT$1600',
+  },
+  {
+    avatar: images.teacher02,
+    name: 'Guava',
+    title: '2022 Python 全攻略｜從入門到實務',
+    type: '後端語言',
+    content:
+      'Python支援多種程式設計範式，包括函數式、指令式、反射式、結構化和物件導向程式設計，它擁有動態型別系統和垃圾回收功能，能夠自動管理記憶體使用，本堂課帶你無痛入門，開發簡單的應用！',
+    time: '50分鐘',
+    price: 'NT$2200',
+  },
+  {
+    avatar: images.teacher03,
+    name: 'Joe',
+    title: '活用 Line Bot ｜APP 開發到上架完整實戰攻略',
+    type: '行動應用開發',
+    content:
+      'Line Bot 可用於實用的目的，如客戶服務或資訊獲取，有些聊天機器人會搭載自然語言處理系統，但大多簡單的系統只會擷取輸入的關鍵字，再從語料庫中找尋最合適的應答句，本堂課會帶你了解 Line Bot 如何結合其他工具的應用。',
+    time: '50分鐘',
+    price: 'NT$1800',
+  },
+  {
+    avatar: images.teacher04,
+    name: 'Lina',
+    title: 'Linux 零基礎七天入門｜入門到實務',
+    type: '作業系統與伺服器',
+    content:
+      'Linux是一種自由和開放原始碼的類UNIX作業系統，作業系統中包含了許多使用者圖形介面和其他實用工具。課程會一步一步帶著你走，讓你能夠從頭開始學習程式語言的基本語法',
+    time: '50分鐘',
+    price: 'NT$1100',
+  },
+]
+
+export default data
--- src/pages/home/components/PopularCourses.tsx
+++ src/pages/home/components/PopularCourses.tsx
@@ -1,50 +1,10 @@
 import Title from '@/components/Title'
-import GradientButton from '@/components/GradientButton'
-import images from '@/lib/images'
+import Button from '@/components/Button'
+import courses from '@/lib/courseData'
 
 const PopularCourses: React.FC = () => {
-  const courses = [
-    {
-      avatar: images.teacher01,
-      name: 'Belinda',
-      title: '基礎前端 - HTML',
-      type: '前端語言',
-      content:
-        '帶領無基礎的平民老百姓，從最基礎的 HTML 語法出發，使用最淺顯易懂的語言與生活隨處可見...',
-      time: '50分鐘',
-      price: 'NT$1600',
-    },
-    {
-      avatar: images.teacher02,
-      name: 'Guava',
-      title: '2022 Python 全攻略｜從入門到實務',
-      type: '後端語言',
-      content:
-        'Python支援多種程式設計範式，包括函數式、指令式、反射式、結構化和物件導向程式設計...',
-      time: '50分鐘',
-      price: 'NT$2200',
-    },
-    {
-      avatar: images.teacher03,
-      name: 'Joe',
-      title: '活用 Line Bot ｜APP 開發到上架完整實戰攻略',
-      type: '行動應用開發',
-      content:
-        'Line Bot 可用於實用的目的，如客戶服務或資訊獲取，有些聊天機器人會搭載自然語言...',
-      time: '50分鐘',
-      price: 'NT$1800',
-    },
-    {
-      avatar: images.teacher04,
-      name: 'Lina',
-      title: 'Linux 零基礎七天入門｜入門到實務',
-      type: '作業系統與伺服器',
-      content:
-        'Linux是一種自由和開放原始碼的類UNIX作業系統，作業系統中包含了許多使用者圖形介面...',
-      time: '50分鐘',
-      price: 'NT$1100',
-    },
-  ]
+  const navigate = useNavigate()
+
   return (
     <div className="bg-dark pb-10 sm:pb-20">
       <Title title={'熱門推薦課程'} />
@@ -54,9 +14,10 @@ const PopularCourses: React.FC = () => {
             <li
               className="cursor-pointer rounded-xl border border-transparent bg-primary pt-6 pb-4 hover:border-secondary"
               key={item.title}
+              onClick={() => navigate('/courseIntroduction')}
             >
               <img
-                className="w-100[px] mx-auto mb-2 h-[100px] rounded-full"
+                className="mx-auto mb-2 h-[100px] w-[100px] rounded-full"
                 src={item.avatar}
                 alt="avatar"
               />
@@ -66,7 +27,7 @@ const PopularCourses: React.FC = () => {
                   {item.title}
                 </p>
                 <p className="mb-2 text-secondary">{item.type}</p>
-                <p>{item.content}</p>
+                <p className="text-hidden">{item.content}</p>
               </div>
               <div className="border-t border-white pt-4 text-center">
                 <p className="text-3xl">
@@ -79,7 +40,12 @@ const PopularCourses: React.FC = () => {
         })}
       </ul>
       <div className="mt-4 text-center sm:mt-7">
-        <GradientButton text={'所有課程列表'} px={32} />
+        <Button
+          text="所有課程列表"
+          padding="py-2 px-8"
+          gradient={true}
+          onClick={() => navigate('/courseIntroduction')}
+        />
       </div>
     </div>
   )
