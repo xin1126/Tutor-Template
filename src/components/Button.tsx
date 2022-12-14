66da1474b73376731a296b56a04bc8f8bb202655
xin1126 <z26319491z@gmail.com>
2022-12-14 10:59:50 +0800

    fix: Button Components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,39 +1,16 @@
-import { ReactNode } from 'react'
 interface Props {
-  color: string
-  children: ReactNode
-  click: () => void
+  text: string
+  click?: () => void
 }
 
-const Button: React.FC<Props> = ({ color, click, children }) => {
-  const [bg, setBg] = useState('primary')
-
-  useEffect(() => {
-    switch (color) {
-      case 'primary':
-        setBg('bg-primary')
-        break
-      case 'secondary':
-        setBg('bg-secondary')
-        break
-      case 'success':
-        setBg('bg-success')
-        break
-      case 'warning':
-        setBg('bg-warning')
-        break
-      case 'danger':
-        setBg('bg-danger')
-        break
-    }
-  }, [color])
+const Button: React.FC<Props> = ({ text, click }) => {
   return (
     <button
       type="button"
-      className={`rounded-lg p-2 text-white transition duration-700 hover:opacity-80 ${bg} mr-3 w-[100px]`}
+      className="min-w-[130px] rounded-xl border border-white py-2 px-8 text-white transition duration-500 hover:border-secondary"
       onClick={click}
     >
-      {children}
+      {text}
     </button>
   )
 }
